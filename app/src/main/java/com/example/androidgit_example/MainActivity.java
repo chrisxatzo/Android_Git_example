2a17b80aa41b21b46e17dcfbd430287832b1db65
KotrotsKon <kotrotskon@users.noreply.github.com>
2020-04-06 18:22:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidgit_example/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.androidgit_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtVw_kostas = findViewById(R.id.txtVw_kostas);
+        txtVw_kostas.setText("Kostas");
+    }
+}
